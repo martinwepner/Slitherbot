b982d756ad0bc73c5401af2788520617d785dc35
karl.kraus <laqueray@googlemail.com>
2016-06-10 00:52:43 +0200

    bot rewrite (no snake collision detection)

--- slitherbot.js
+++ slitherbot.js
@@ -1,245 +1,165 @@
-if (window.xxx_iv_)clearInterval(xxx_iv_);
-xxx_iv_=setInterval(function()
+if (window.xxx_iv_)clearInterval(window.xxx_iv_);
+
+(function()
 {
-  if (!snake) return;
-  
-  var fx=Math.cos(snake.ang);
-  var fy=Math.sin(snake.ang);
-  var a, lookahead=0;
-	var vectors = foods
-    .filter(x=>x)
-		.map(x=>(a=[x.xx-snake.xx, x.yy-snake.yy, 0, 0], a[2] = Math.hypot(a[0],a[1]), a[3] = x.rad, a))
-    .filter(x=>
+  class v2
+  {
+    constructor(x, y)
     {
-      if (Math.hypot(x[0], x[1]-20+snake.tl*1.1) < Math.max(snake.tl, 25) + 30)
+      if (isNaN(x))
       {
-        return false;
-      }
-      return true;
-    })
-    .sort((a,b)=>a[2]/a[3] - b[2]/b[3])
-    ;
-  
-	var vectors2 = preys
-    .filter(x=>x)
-		.map(x=>(a=[x.xx-snake.xx,x.yy-snake.yy,0],a[2]=Math.hypot(a[0],a[1]),a))
-    .filter(x=>{
-      if (Math.hypot(x[0], x[1]-20+snake.tl*1.1) < 30+snake.tl)
-      {
-        return false;
-      }
-      return true;
-    })
-    .sort((a,b)=>a[2]-b[2])
-    ;
-  
-  xm = 0;
-  ym = 0;
-  weight = 0.1;
-  
-  var vmoving = vectors2.filter(x=>x[2]<800);
-  if (vmoving.length && (snake.tl > 6 || (snake.tl > 3 && snake.wmd)) && (snake.tl <= 70 || vmoving[0][2] < 150))
-  {
-    var target = vmoving[0];
-    
-    weight += 10;
-    xm = target[0]*10;
-    ym = target[1]*10;
-    snake.wmd = true;
-    snake._hunt = true;
-  }
-  else if (vectors.length > lookahead)
-  {
-    var best = vectors[lookahead];
-    
-    weight += 150;
-    xm = best[0]*150;
-    ym = best[1]*150;
-    lookahead = 0;
-    snake.wmd = false;
-    snake._hunt = false;
-  }
-  else
-  {
-    snake.wmd = false;
-    snake._hunt = false;
-  }
-
-  var cDx = 1234567;
-  var cDy = 1234567;
-  var cPx = 1234567;
-  var cPy = 1234567;
-  var cPr = 1234567;
-  var cR = 1234567;
-  var cWMD = false;
-
-  snakes.forEach(other=>
-  {
-    if(snake==other)return;
-
-    /*var pt = other;
-    var dx = pt.xx-snake.xx, dy=pt.yy-snake.yy;
-    //Kopf ausweichen
-    var r = -Math.hypot(dx, dy) - other.tl - snake.tl * 1.5;
-    
-    var maxdist = 130 + other.tl
-    if (r < maxdist)
-    {
-      
-      xm = dx;
-      ym = dy;
-    }*/
-    
-    other.pts
-    .map((pt,i,a)=>
-    {
-      var dx = pt.xx-snake.xx, dy=pt.yy-snake.yy;
-      var r = Math.hypot(dx, dy);
-      
-      pt._dx = dx;
-      pt._dy = dy;
-      pt._r = r;
-
-      var p1, p2;
-      
-
-      if(i == a.length-1)
-      {
-        p1 = a[i - 1];
-        p2 = a[i];
+        this.set(x);
       }
       else
       {
-        p1 = a[i];
-        p2 = a[i + 1];
+        this.x = x || 0;
+        this.y = y || 0;
       }
-      pt._px = p1.xx-p2.xx;
-      pt._py = p1.yy-p2.yy;
-      pt._pr = Math.hypot(pt._px, pt._py);
-
-      /*if (r < Math.max(snake.tl*2, 200) && snake.wmd)
-      {
-        snake.wmd = false;
-      }*/
-
-      if(pt._r < cR)
-      {
-        cDx = pt._dx;
-        cDy = pt._dy;
-        cPx = pt._px;
-        cPy = pt._py;
-        cPr = pt._pr;
-        cR = pt._r, cR;
-        cWMD = other.tsp > other.fsp;
-      }
-      
-      return pt;
-    })/*
-    .sort((a, b)=>{
-      return a._r - b._r;
-    })
-    .filter(x=>x)
-    .slice(0, 1)
-    .forEach((pt,i,a)=>{
-      
-      var r = pt._r;
-      //r -= other.tl;
-      if (snake.wmd)
-      {
-        r += snake.tl;
-      }
-
-      var safeDistance = Math.max(snake.tl * 2.3, 300);
-      if (r < safeDistance)
-      {
-        var dx = pt._dx;
-        var dy = pt._dy;
-        
-        var f1 = 0;
-        var f2 = 5000;
-
-        if(r < Math.max(other.tl*2.5, 150))
-        {
-          f1 = -5000;
-          f2 = 0;
-        }
-
-        var reldot = (pt._px*fx + pt._py*fy) / pt._pr;
-        if (reldot < 0)
-        {
-          f2 = -f2;
-        }
-
-        dx = pt._dx * f1 + pt._px * f2;
-        dy = pt._dy * f1 + pt._py * f2;
-
-        //r = Math.hypot(dx, dy);
-        
-        //var w = Math.pow((snake.tl + safeDistance - r), 3) * 1.5;
-          
-        xm += dx;//*w;
-        ym += dy;//*w;
-        //weight += w;*/
-        
-        /*if (r < 2000 && !snake.wmd)
-        {
-          var w = Math.pow((2000 - r) * 0.001, 1);
-          
-          xm -= dx*w;
-          ym -= dy*w;
-          weight += w;
-        }
-      }
-    });*/
-  });      
-
-  var r = cR;
-  //r -= other.tl;
-  if (snake.wmd)
-  {
-    r -= snake.tl/2;
-  }
-
-  var safeDistance = Math.max(snake.tl * 3, 300);
-  safeDistance *= snake.wmd ? 3 : 1;
-  safeDistance /= snake._hunt ? 2 : 1;
-  if (r < safeDistance)
-  {
-    snake._hunt = false;
-    var dx = cDx;
-    var dy = cDy;
-    var direction = 1;
-
-    var reldot = (cPx*fx + cPy*fy) / cPr;
-    if (reldot < 0)
-    {
-      direction = -1;
     }
     
-    var f1 = -0.3;
-    var f2 = 1 * direction;
-
-    if(cWMD && r > Math.max(50, snake.tl * 2))
+    set(v)
     {
-      f1 = -0;
-      f2 = 1 * direction;
-      if(direction)
-        snake.wmd = true;
-      else
-        snake.wmd = true;
+      this.x = v.x;
+      this.y = v.y;
+      return this;
     }
-    else
-      snake.wmd = false;
-
-    dx = cDx * f1 + cPx * f2;
-    dy = cDy * f1 + cPy * f2;
-
-    //r = Math.hypot(dx, dy);
     
-    //var w = Math.pow((snake.tl + safeDistance - r), 3) * 1.5;
-      
-    xm = dx;//*w;
-    ym = dy;//*w;
+    dup()
+    {
+      return new this.constructor(this);
+    }
+    
+    mad(v, f)
+    {
+      this.x += v.x * f;
+      this.y += v.y * f;
+      return this;
+    }
+    
+    mul(f)
+    {
+      this.x *= f;
+      this.y *= f;
+      return this;
+    }
+    
+    cross(v)
+    {
+      return this.x*v.y - this.y*v.x;
+    }
+    
+    dot(v)
+    {
+      return this.x*v.x + this.y*v.y;
+    }
+    
+    length()
+    {
+      return Math.hypot(this.x, this.y);
+    }
+    
+    unit()
+    {
+      return this.mul(1/this.length());
+    }
+    
+    *iter()
+    {
+      yield this.x;
+      yield this.y;
+    }
+    
+    toString()
+    {
+      return "<" + this.x + " " + this.y + ">";
+    }
   }
   
-}, 100);
+  var __state =
+  {
+    probes: [],
+  };
+  
+  window.xxx_iv_=setInterval(function()
+  {
+    if (!snake) return;
+    
+    var objects = new Map();
+    
+    var foods = window.foods
+    .filter((food)=>food)
+    .map((food)=>
+    {
+      var out = new v2(food.rx, food.ry);
+      
+      out.id = food.id;
+      out.size = food.sz;
+      
+      objects.set(out.id, out);
+      
+      return out;
+    });
+    
+    var snakes = window.snakes
+    .filter((snake)=>snake)
+    .map((snake)=>
+    {
+      var out = {};
+      
+      out.id = snake.id;
+      out.head = new v2(snake.xx, snake.yy);
+      out.tail = snake.pts.map((point, i, a)=>
+      {
+        var out = new v2(point.xx, point.yy);
+        
+        var last = a[i-1] || point;
+        var next = a[i+1] || point;
+        
+        out.tangent = new v2(last.xx - next.xx, last.yy - next.yy).unit();
+        
+        return out;
+      });
+      
+      out.speed = snake.sp;
+      out.heading = new v2(
+        Math.cos(snake.ang),
+        Math.sin(snake.ang)
+      );
+      
+      objects.set(out.id, out);
+      
+      return out;
+    });
+    
+    var me = objects.get(snake.id);
+    if (!me)
+    {
+      return;
+    }
+    
+    var best = foods
+    .filter((food)=>
+    {
+      var offset = food.dup().mad(me.head, -1);
+      var distance = offset.length();
+      
+      var front = offset.dot(me.heading);
+      var right = offset.cross(me.heading);
+      
+      var angle = Math.abs(Math.atan2(right, front));
+      
+      food.offset = offset;
+      food.angle = angle;
+      food.distance = distance;
+      
+      return distance < 300 && angle < Math.PI / 3;
+    })
+    .sort((a, b)=>((b.size-a.size) || (a.angle-b.angle)));
+    
+    if (best.length > 0)
+    {
+      xm = best[0].offset.x;
+      ym = best[0].offset.y;
+    }
+  }, 100);
+})();
